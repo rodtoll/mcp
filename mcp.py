7dfd359854d2db0284a64ea0c01890f7ff8fad83
Rod Toll <rodtoll@outlook.com>
2014-12-29 04:28:54 +0000

    removed password

--- mcp.py
+++ mcp.py
@@ -3,6 +3,7 @@ __author__ = 'rodtoll'
 import ISY
 import threading
 import datetime
+import logging
 
 MCP_STATE_HOME = 1
 MCP_STATE_DOOR_OPEN = 2
@@ -25,11 +26,28 @@ DEVICE_STATE_ON = "on"
 BUFFER_TIMEOUT = 180.0
 NO_ACTIVITY_TIMEOUT = 180.0
 
-def isy_event_handler(*args):
-    print "!!! Woke up... by device: "+myisy[args[0]['node']]['name']
-    args[1].set()
+myisy = None
+isy_event = None
+car_device = None
+door_open_device = None
+recent_light_device = None
+phone_present_device = None
+device_active_device = None
+alarm_set_device = None
+logger = None
 
-myisy = ISY.Isy(addr="10.0.1.19", userp="ErgoFlat91", userl="admin", eventupdates=1)
+def log(message):
+    if logger == None:
+        print message
+    else:
+        logger.error(message)
+
+def isy_event_handler(*args):
+    global myisy
+    global isy_event 
+
+    log("!!! Woke up... by device: "+myisy[args[0]['node']]['name'])
+    isy_event.set()
 
 current_state = MCP_STATE_DOOR_CLOSED_SENSING_FULL
 
@@ -92,28 +110,13 @@ def get_current_state_as_string():
 
     return state_string
 
-isy_event = threading.Event()
-myisy.callback_set(DEVICE_ID_DOOR_OPEN, isy_event_handler, isy_event)
-myisy.callback_set(DEVICE_ID_DEVICE_ACTIVE, isy_event_handler, isy_event)
-myisy.callback_set(DEVICE_ID_PHONE_PRESENT, isy_event_handler, isy_event)
-myisy.callback_set(DEVICE_ID_RECENT_LIGHT, isy_event_handler, isy_event)
-myisy.callback_set(DEVICE_ID_CAR_PRESENT, isy_event_handler, isy_event)
-myisy.callback_set(DEVICE_ID_ALARM_SET, isy_event_handler, isy_event)
-
-car_device = myisy[DEVICE_ID_CAR_PRESENT]
-door_open_device = myisy[DEVICE_ID_DOOR_OPEN]
-recent_light_device = myisy[DEVICE_ID_RECENT_LIGHT]
-phone_present_device = myisy[DEVICE_ID_PHONE_PRESENT]
-device_active_device = myisy[DEVICE_ID_DEVICE_ACTIVE]
-alarm_set_device = myisy[DEVICE_ID_ALARM_SET]
-
 last_state_change = datetime.datetime.now()
 
 def change_current_state(new_state):
     global current_state
     global last_state_change
 
-    print "Transition from "+state_as_string(current_state)+" to "+state_as_string(new_state)
+    log("Transition from "+state_as_string(current_state)+" to "+state_as_string(new_state))
     last_state_change = datetime.datetime.now()
     current_state = new_state
 
@@ -182,87 +185,87 @@ def check_for_recent_light_activity():
 
 def handle_state_home():
     if check_door_open():
-        print "!!! Door is open transitioning..."
+        log("!!! Door is open transitioning...")
         change_current_state(MCP_STATE_DOOR_OPEN)
     elif check_alarm_away_active():
-        print "!!! Alarm is active in away mode, ignoring other signals and transitioning..."
+        log("!!! Alarm is active in away mode, ignoring other signals and transitioning...")
         change_current_state(MCP_STATE_AWAY)
     return
 
 def handle_state_door_open():
     if not check_door_open():
-        print "!!! Door is now closed, transitioning..."
+        log("!!! Door is now closed, transitioning...")
         change_current_state(MCP_STATE_DOOR_CLOSED_SENSING_BUFFER)
     return
 
 def handle_state_door_closed_sensing_buffer():
     if check_door_open():
-        print "!!! Door was opened, trasitioning..."
+        log("!!! Door was opened, trasitioning...")
         change_current_state(MCP_STATE_DOOR_OPEN)
     elif get_delta_from_state_change_s() >= BUFFER_TIMEOUT:
-        print "!!! Buffer state has expired, transitioning..."
+        log("!!! Buffer state has expired, transitioning...")
         change_current_state(MCP_STATE_DOOR_CLOSED_SENSING_FULL)
 
 def handle_state_door_closed_sensing_full():
     if check_door_open():
-        print "!!! Door was opened, transitioning..."
+        log("!!! Door was opened, transitioning...")
         change_current_state(MCP_STATE_DOOR_OPEN)
     elif check_alarm_set():
         if check_alarm_away_active():
-            print "!!! Alarm is active in away mode, ignoring other signals and transitioning..."
+            log("!!! Alarm is active in away mode, ignoring other signals and transitioning...")
             change_current_state(MCP_STATE_AWAY)
         elif check_alarm_stay_active():
-            print "!!! Alarm is active in stay mode, transitioning..."
+            log("!!! Alarm is active in stay mode, transitioning...")
             change_current_state(MCP_STATE_HOME)
     elif check_for_recent_light_activity():
-        print "!!! Activity in the Insteon light switches, transitioning..."
+        log("!!! Activity in the Insteon light switches, transitioning...")
         change_current_state(MCP_STATE_HOME)
     elif check_phone_present() or check_recent_device():
-        print "!!! Phone is present in range of the network, transitioning..."
+        log("!!! Phone is present in range of the network, transitioning...")
         change_current_state(MCP_STATE_HOME_BY_DEVICE)
     elif get_delta_from_state_change_s() >= NO_ACTIVITY_TIMEOUT:
-        print "!!! No activity, no one must be home..."
+        log("!!! No activity, no one must be home...")
         change_current_state(MCP_STATE_AWAY)
 
 def handle_state_home_by_device():
     if check_door_open():
-        print "!!! Door was opened, transitioning..."
+        log("!!! Door was opened, transitioning...")
         change_current_state(MCP_STATE_DOOR_OPEN)
     elif check_alarm_set():
         if check_alarm_away_active():
-            print "!!! Alarm is active in away mode, ignoring other signals and transitioning..."
+            log("!!! Alarm is active in away mode, ignoring other signals and transitioning...")
             change_current_state(MCP_STATE_AWAY)
         elif check_alarm_stay_active():
-            print "!!! Alarm is active in stay mode, transitioning..."
+            log("!!! Alarm is active in stay mode, transitioning...")
             change_current_state(MCP_STATE_HOME)
     elif check_for_recent_light_activity():
-        print "!!! Activity in the Insteon light switches, transitioning..."
+        log("!!! Activity in the Insteon light switches, transitioning...")
         change_current_state(MCP_STATE_HOME)
     elif check_recent_device():
-        print "!!! Activity in monitored devices, transitioning..."
+        log("!!! Activity in monitored devices, transitioning...")
         change_current_state(MCP_STATE_HOME)
     elif not check_phone_present():
-        print "!!! Phone is no longer present, must have been a ghost ping?"
+        log("!!! Phone is no longer present, must have been a ghost ping?")
         change_current_state(MCP_STATE_AWAY)
 
 def handle_state_away():
     if check_door_open():
-        print "!!! Door was opened, transitioning..."
+        log("!!! Door was opened, transitioning...")
         change_current_state(MCP_STATE_DOOR_OPEN)
     elif check_alarm_set():
         if check_alarm_away_active():
-            print "!!! Alarm is set to away, ignoring other signals"
+            log("!!! Alarm is set to away, ignoring other signals")
         elif check_alarm_stay_active():
-            print "!!! Alarm is set to stay, ignoring other signals and transitioning..."
+            log("!!! Alarm is set to stay, ignoring other signals and transitioning...")
             change_current_state(MCP_STATE_HOME)
     elif check_for_recent_light_activity():
-        print "!!! Activity in the Insteon light switches, transitioning..."
+        log("!!! Activity in the Insteon light switches, transitioning...")
         change_current_state(MCP_STATE_HOME)
     elif check_recent_device():
-        print "!!! Activity on monitored devices, transitioning..."
+        log("!!! Activity on monitored devices, transitioning...")
         change_current_state(MCP_STATE_HOME)
     elif check_phone_present():
-        print "!!! Phone is present in range of the network, transitioning..."
+        log("!!! Phone is present in range of the network, transitioning...")
         change_current_state(MCP_STATE_HOME_BY_DEVICE)
 
 def set_home_indicators(someone_home):
@@ -278,7 +281,7 @@ def check_state():
 
     start_state = current_state
 
-    print "+++ "+get_current_state_as_string()
+    log("+++ "+get_current_state_as_string())
     if current_state == MCP_STATE_HOME:
         handle_state_home()
     elif current_state == MCP_STATE_DOOR_OPEN:
@@ -291,7 +294,7 @@ def check_state():
         handle_state_home_by_device()
     elif current_state == MCP_STATE_AWAY:
         handle_state_away()
-    print "--- "+get_current_state_as_string()
+    log("--- "+get_current_state_as_string())
 
     set_home_indicators(current_state == MCP_STATE_HOME)
 
@@ -315,18 +318,63 @@ def get_wait_period():
         if result < 0.0:  # Special case where we checked earlier for timeout and wasn't quite there but now it is
             result = 1.0
 
-    print "!!! sleeping for: "+str(result)
+    log("!!! sleeping for: "+str(result))
     return result
 
-while True:
-    print ">>> "+get_current_state_as_string()
+def init_devices():
+    global myisy
+    global car_device
+    global door_open_device
+    global recent_light_device
+    global phone_present_device
+    global alarm_set_device
+    global device_active_device
+
+    car_device = myisy[DEVICE_ID_CAR_PRESENT]
+    door_open_device = myisy[DEVICE_ID_DOOR_OPEN]
+    recent_light_device = myisy[DEVICE_ID_RECENT_LIGHT]
+    phone_present_device = myisy[DEVICE_ID_PHONE_PRESENT]
+    device_active_device = myisy[DEVICE_ID_DEVICE_ACTIVE]
+    alarm_set_device = myisy[DEVICE_ID_ALARM_SET]
+
+def init_isy():
+    global myisy
+
+    myisy = ISY.Isy(addr="10.0.1.19", userp="", userl="admin", eventupdates=1)
+
+def init_events():
+    global isy_event
+    global myisy
+
+    isy_event = threading.Event()
+    myisy.callback_set(DEVICE_ID_DOOR_OPEN, isy_event_handler)
+    myisy.callback_set(DEVICE_ID_DEVICE_ACTIVE, isy_event_handler)
+    myisy.callback_set(DEVICE_ID_PHONE_PRESENT, isy_event_handler)
+    myisy.callback_set(DEVICE_ID_RECENT_LIGHT, isy_event_handler)
+    myisy.callback_set(DEVICE_ID_CAR_PRESENT, isy_event_handler)
+    myisy.callback_set(DEVICE_ID_ALARM_SET, isy_event_handler)
+
+def daemon_main(main_logger):
+    global isy_event
+    global logger
+    
+    logger = main_logger
+
+    init_isy()
+    init_events()
+    init_devices()
 
     while True:
-        if not check_state():
-            break
+        log(">>> "+get_current_state_as_string())
 
-    print "<<< "+get_current_state_as_string()
+        while True:
+            if not check_state():
+                break
 
-    isy_event.wait(get_wait_period())
-    isy_event.clear()
+        log("<<< "+get_current_state_as_string())
 
+        isy_event.wait(get_wait_period())
+        isy_event.clear()
+
+if __name__ == "__main__":
+    daemon_main(None)
