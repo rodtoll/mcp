46fea1f388d8be4278e90029e40685b172628d71
Rod Toll <rodtoll@outlook.com>
2014-12-29 18:15:18 +0000

    Moved from python-daemon

--- mcp_main.py
+++ mcp_main.py
@@ -1,42 +1,24 @@
-import time
-import mcp
-import logging
-import os
-import sys
-import traceback
+import sys, time, mcp
+from daemon import Daemon
 
-from daemon import runner
+class MyDaemon(Daemon):
+	def run(self):
+            mcp.daemon_main(self)
 
-class App():
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/home/root/mcp/mcp.stdout.log'
-        self.stderr_path = '/home/root/mcp/mcp.stderr.log'
-        self.pidfile_path =  '/home/root/mcp/mcp.pid'
-        self.logfile_path = '/home/root/mcp/mcp.log'
-        self.pidfile_timeout = 5
+if __name__ == "__main__":
+	daemon = MyDaemon('/home/root/mcp/mcp.pid')
+	if len(sys.argv) == 2:
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		else:
+			print "Unknown command"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		print "usage: %s start|stop|restart" % sys.argv[0]
+		sys.exit(2)
 
-    def log_message(self,msg):
-        self.logger.error(msg)
-
-    def init_logger(self):
-        self.logger = logging.getLogger("DaemonLog")
-        self.logger.setLevel(logging.INFO)
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        self.handler = logging.FileHandler(self.logfile_path)
-        self.handler.setFormatter(formatter)
-        self.logger.addHandler(self.handler)
-
-    def run(self):
-        self.init_logger()
-        self.logger.error("Start")
-        try:
-            mcp.daemon_main(self.logger)
-        except:
-            tb = traceback.format_exc()
-            self.logger.error("FAILED!!!!! "+tb)
-        self.logger.error("Done")
-
-app = App()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
